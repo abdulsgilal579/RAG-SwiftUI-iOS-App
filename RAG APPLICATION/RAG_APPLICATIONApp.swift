dc00737c608ca8acf3ebd8c07a172c52fdd02e65
Abdul Samad Gilal <137406778+abdulsgilal579@users.noreply.github.com>
2026-06-13 21:45:09 +0500

    Initial Commit

---
+++ RAG APPLICATION/RAG_APPLICATIONApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAG_APPLICATIONApp.swift
+//  RAG APPLICATION
+//
+//  Created by Abdul Samad Gilal on 13/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RAG_APPLICATIONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
